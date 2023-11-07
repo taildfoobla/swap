752c692a410dc16be37f9f377e58912bed2fa5cb
taild <taild@foobla.com>
2023-11-07 09:37:59 +0700

    update 07/11

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+
+const apps = [
+    {
+        "name": "Swap-BSC",
+        "script": "sqd",
+        "args" : "process:bsc"
+    },
+    {
+        "name": "Swap-Eth",
+        "script": "sqd",
+        "args" : "process:eth"
+    }
+
+];
+
+module.exports = apps;
