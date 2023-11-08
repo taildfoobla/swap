b4a4248c42329bab17c2019cc9fd4bf788b59a3a
taild <taild@foobla.com>
2023-11-08 15:02:31 +0700

    update 08/11

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,21 +3,31 @@ import fs from "fs"
 
 
 export const renderInOutToken=(amount0In:BigInt,amount0Out:BigInt,amount1In:BigInt,amount1Out:BigInt,address0:String,address1:String)=>{
-    if(amount0In.toString()!=="0"){
-        return {
-            amount0:amount1Out,
-            amount1:amount0In,
-            token0:address1,
-            token1:address0,
+    if(amount0In&&amount0Out&&amount1In&&amount1Out){
+        if(amount0In.toString()!=="0"){
+            return {
+                amount0:amount1Out,
+                amount1:amount0In,
+                token0:address1,
+                token1:address0,
+            }
+        }else{
+            return {
+                amount0:amount0Out,
+                amount1:amount1In,
+                token0:address0,
+                token1:address1,
+            }
         }
     }else{
-        return {
-            amount0:amount0Out,
-            amount1:amount1In,
-            token0:address0,
-            token1:address1,
+        return{
+            amount0:"",
+            amount1:"",
+            token0:"",
+            token1:""
         }
     }
+   
 }
 
 
