4cc187efb80a909409693a9cfe41bb4eb8fa4285
taild <taild@foobla.com>
2023-11-17 10:55:54 +0700

    update 17/11

---
+++ ecosystem_pool.config.js
@@ -0,0 +1,16 @@
+
+const apps = [
+    {
+        "name": "ETH_Uniswap_v2_pool",
+        "script": "sqd",
+        "args" : "process:eth_uniswap_v2_pool"
+    },
+    {
+        "name": "ETH_Uniswap_v3_pool",
+        "script": "sqd",
+        "args" : "process:eth_uniswap_v3_pool"
+    }
+
+];
+
+module.exports = apps;
