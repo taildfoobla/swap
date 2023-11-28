70d255c9d03432473494b4a7b7ff5ed6910b37ce
dev <taiht@foobla.com>
2023-11-28 16:37:12 +0700

    Update sushiswap

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,11 @@ const apps = [
         "name": "ETH_Uniswap_v3",
         "script": "sqd",
         "args" : "process:eth_uniswap_v3_swap"
+    },
+    {
+        "name": "ETH_Sushiswap_v2",
+        "script": "sqd",
+        "args" : "process:eth_sushiswap_v2_swap"
     }
 
 ];
---
+++ src/eth_sushiswap_v2_swap/processor.ts
@@ -0,0 +1,77 @@
+import {assertNotNull} from '@subsquid/util-internal'
+import {lookupArchive} from '@subsquid/archive-registry'
+import {
+    BlockHeader,
+    DataHandlerContext,
+    EvmBatchProcessor,
+    EvmBatchProcessorFields,
+    Log as _Log,
+    Transaction as _Transaction,
+} from '@subsquid/evm-processor'
+import {Store} from '@subsquid/typeorm-store'
+import { factoryAddress } from '../address/factory_address'
+import * as erc20abi from '../abi/erc20'
+import * as uniswapV3EthFactoryAbi from '../abi/uniswap_v3_ethereum_factory'
+import * as uniswapV3EthPoolAbi from '../abi/uniswap_v3_ethereum_pool'
+import * as uniswapV2EthFactoryAbi from '../abi/uniswap_v2_ethereum_factory'
+import * as uniswapV2EthPoolAbi from '../abi/uniswap_v2_ethereum_pool'
+
+import * as sushiV2EthFactoryAbi from "../abi/sushiswap_v2_ethereum_factory"
+import * as sushiV2EthPoolAbi from "../abi/sushiswap_v2_ethereum_pool"
+
+
+function renderFactoryAddress (){
+    const data:string[] =[]
+    factoryAddress.forEach((item:any)=>{
+        if(item?.network==="ethereum"){
+            data.push(item?.address?.toLowerCase())
+        }
+    })
+    return data
+}
+
+export const ETH_ADDRESS:string ='0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac'.toLowerCase()
+
+export const processor = new EvmBatchProcessor()
+    .setDataSource({
+        // Lookup archive by the network name in Subsquid registry
+        // See https://docs.subsquid.io/evm-indexing/supported-networks/
+        archive: lookupArchive('eth-mainnet'),
+        // Chain RPC endpoint is required for
+        //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
+        //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
+        chain: {
+            // Set via .env for local runs or via secrets when deploying to Subsquid Cloud
+            // https://docs.subsquid.io/deploy-squid/env-variables/
+            url: assertNotNull(process.env.RPC_ENDPOINT_ETH),
+            // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
+            rateLimit: 10
+        }
+    })
+    .setFinalityConfirmation(75)
+    .setFields({
+        log: {
+            transactionHash: true
+        },
+        transaction: {
+            from: true,
+            to: true
+          }
+    })
+    .setBlockRange({
+        from: 10794228,
+    })
+    .addLog({
+        address: [ETH_ADDRESS],
+        topic0: [sushiV2EthFactoryAbi.events.PairCreated.topic],
+    })
+    .addLog({
+        topic0: [sushiV2EthPoolAbi.events.Swap.topic],
+        transaction: true,
+      });
+
+export type Fields = EvmBatchProcessorFields<typeof processor>
+export type Context = DataHandlerContext<Store, Fields>
+export type Block = BlockHeader<Fields>
+export type Log = _Log<Fields>
+export type Transaction = _Transaction<Fields>
