a788959a6801cbb690114c82bf675b42fa4cfb04
taiht <taiht@foobla.com>
2024-01-23 16:00:10 +0700

    Update 2024-01-23

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,16 +30,16 @@ const apps = [
     //     "script": "sqd",
     //     "args" : "process:eth_balancer_v1_swap"
     // },
-    // {
-    //     "name": "ETH_Balancer_v2",
-    //     "script": "sqd",
-    //     "args" : "process:eth_balancer_v2_swap"
-    // },
     {
-        "name": "BSC_Pancakeswap_v3",
+        "name": "ETH_Balancer_v2",
         "script": "sqd",
-        "args" : "process:bsc_pancakeswap_v3_swap"
-    },
+        "args" : "process:eth_balancer_v2_swap"
+    }
+    // {
+    //     "name": "BSC_Pancakeswap_v3",
+    //     "script": "sqd",
+    //     "args" : "process:bsc_pancakeswap_v3_swap"
+    // },
 
 ];
 
